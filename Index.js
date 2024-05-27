f66eb9747bd0daa207601c76b441cb35c3b4b886
ABHIJIT\abhij <gadakhabhijit@gmail.com>
2024-05-28 00:49:53 +0530

    commit 04 - home page completed

---
+++ Index.js
@@ -0,0 +1,43 @@
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const carouselInner = document.querySelector(".carousel-inner");
+const items = document.querySelectorAll(".carousel-item");
+const itemsVisible = 5;
+let currentIndex = 0;
+
+const updateCarousel = () => {
+  carouselInner.style.transform = `translateX(-${
+    currentIndex * (100 / itemsVisible)
+  }%)`;
+};
+
+prevButton.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex -= 2;
+    if (currentIndex < 0) currentIndex = 0;
+    updateCarousel();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentIndex < items.length - itemsVisible) {
+    currentIndex += 2;
+    if (currentIndex > items.length - itemsVisible)
+      currentIndex = items.length - itemsVisible;
+    updateCarousel();
+  }
+});
+
+// Initialize carousel position
+updateCarousel();
+
+const searchInput = document.querySelector(".search-input");
+const textInputBox = document.querySelector(".text-input");
+
+searchInput.addEventListener("click", () =>
+  textInputBox.classList.add("theme-border")
+);
+
+searchInput.addEventListener("blur", () =>
+  textInputBox.classList.remove("theme-border")
+);
