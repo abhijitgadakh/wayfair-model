6387296c250e83b0d73caed054165ef138611a16
ABHIJIT\abhij <gadakhabhijit@gmail.com>
2024-05-28 22:03:30 +0530

    Commit 08 - cart slider, blur effect and some button functionality added

--- Index.js
+++ Index.js
@@ -43,3 +43,30 @@ searchInput.addEventListener("click", () =>
 searchInput.addEventListener("blur", () =>
   textInputBox.classList.remove("theme-border")
 );
+
+////////////////////// CART ///////////////////////////
+
+function openCart() {
+  console.log("hello");
+  const blurDiv = document.querySelector(".active-blur");
+  const cartDiv = document.querySelector(".cart-slider");
+  // blurDiv.style.display = "none";
+  cartDiv.classList.remove("hide");
+
+  blurDiv.style.display = "block";
+}
+
+function closeCart() {
+  console.log("hello");
+  const blurDiv = document.querySelector(".active-blur");
+  const cartDiv = document.querySelector(".cart-slider");
+  // blurDiv.style.display = "none";
+  cartDiv.classList.add("hide");
+  blurDiv.style.display = "none";
+}
+
+const cartOpen = document.getElementById("open-cart");
+const cartClose = document.querySelector(".close-icon");
+
+cartOpen.addEventListener("click", openCart);
+cartClose.addEventListener("click", closeCart);
--- products/products.js
+++ products/products.js
@@ -1,36 +1,3 @@
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-const carouselInner = document.querySelector(".carousel-inner");
-const items = document.querySelectorAll(".carousel-item");
-const itemsVisible = 5;
-let currentIndex = 0;
-
-const updateCarousel = () => {
-  carouselInner.style.transform = `translateX(-${
-    currentIndex * (100 / itemsVisible)
-  }%)`;
-};
-
-prevButton.addEventListener("click", () => {
-  if (currentIndex > 0) {
-    currentIndex -= 2;
-    if (currentIndex < 0) currentIndex = 0;
-    updateCarousel();
-  }
-});
-
-nextButton.addEventListener("click", () => {
-  if (currentIndex < items.length - itemsVisible) {
-    currentIndex += 2;
-    if (currentIndex > items.length - itemsVisible)
-      currentIndex = items.length - itemsVisible;
-    updateCarousel();
-  }
-});
-
-// Initialize carousel position
-updateCarousel();
-
 const searchInput = document.querySelector(".search-input");
 const textInputBox = document.querySelector(".text-input");
 
@@ -41,3 +8,30 @@ searchInput.addEventListener("click", () =>
 searchInput.addEventListener("blur", () =>
   textInputBox.classList.remove("theme-border")
 );
+
+////////////////////// CART ///////////////////////////
+
+function openCart() {
+  console.log("hello");
+  const blurDiv = document.querySelector(".active-blur");
+  const cartDiv = document.querySelector(".cart-slider");
+  // blurDiv.style.display = "none";
+  cartDiv.classList.remove("hide");
+
+  blurDiv.style.display = "block";
+}
+
+function closeCart() {
+  console.log("hello");
+  const blurDiv = document.querySelector(".active-blur");
+  const cartDiv = document.querySelector(".cart-slider");
+  // blurDiv.style.display = "none";
+  cartDiv.classList.add("hide");
+  blurDiv.style.display = "none";
+}
+
+const cartOpen = document.getElementById("open-cart");
+const cartClose = document.querySelector(".close-icon");
+
+cartOpen.addEventListener("click", openCart);
+cartClose.addEventListener("click", closeCart);
